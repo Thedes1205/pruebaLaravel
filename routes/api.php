75747145de0f7a56e4512a2a4c42e8a16e160e98
Thedes9573 <gabriel.espino4446@alumnos.udg.mx>
2024-01-15 17:20:19 -0600

    add status and registration data

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/QWERTY',function(){
-return File::all();
+return File::with('status')->with('registration')->get();
 });
